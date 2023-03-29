3c30ccb791decfd36556e60927f5cf6787c4b558
Kazmain Shah <kshah228@illinois.edu>
2023-03-29 18:55:56 -0500

    Cleaned CSS; Added Base Website Design

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,16 @@ function App() {
   return (
     <div className="App">
       <h1>Data Science Clicker</h1>
+
+      <div className='Content'> 
+        <form>
+          <p> Join code:</p>
+          <input type="text" name="name" maxLength={4}/>
+          <p> NetID: </p> 
+          <input type="text" name="name"/>
+          <button className='joinbutton'> Join</button>
+        </form> 
+      </div>
     </div>
   )
 }
