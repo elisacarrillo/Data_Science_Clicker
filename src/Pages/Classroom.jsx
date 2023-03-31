17e177b373eb6b206d8b32b12714cfe9245445ab
Kazmain Shah <kshah228@illinois.edu>
2023-03-31 01:47:17 -0500

    Base code for the classroom

--- src/Pages/Classroom.jsx
+++ src/Pages/Classroom.jsx
@@ -1,16 +1,46 @@
-import react from 'react';
+import React from 'react';
+import './Classroom.css'
+
+
+const numChoices = (numButtons) => {
+    const buttons = [];
+    
+    for (let i = 0; i < numButtons; i++) {
+      const label = String.fromCharCode(65 + i);
+      buttons.push(<button key={label} className='MCbutton'>{label}</button>);
+    }
+    
+    return <div className='buttoncontainer'>{buttons}</div>;
+  }
+
 
 const Classroom = () => {
+
+  return (
     
-    return (
-    <div className="App">
-        <p>Welcome to your Classroom </p>
-        <button>Create Question</button>
-        <br></br>
-        <button>View Questions</button>
-        
+    <div className="classroom">
+      <div className='topbox'> 
+        <h1> Classroom A</h1>
+        <h1> Room: PSPA</h1>
+      </div>
+      
+      <div className='contentbox'>
+        <div className='statbox'>
+          <h1> Your Grade</h1>
+          
+          <div className='pichart'></div> 
+        </div>
+        <div className='questionbox'>
+          <h1> Live Question</h1>
+          <div className='question'>
+            <h1> How many States are in the United States of America? </h1>
+          </div>
+          {numChoices(5)}
+        </div>
+      </div>
     </div>
-    );
+  );
 }
+
 
 export default Classroom;
