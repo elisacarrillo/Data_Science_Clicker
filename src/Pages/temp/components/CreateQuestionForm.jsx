2317a96fe9356a5c6d7aa744255ad084ce104ee9
Kazmain Shah <kshah228@illinois.edu>
2023-04-23 15:56:48 -0500

    Finished CreateQuestion Page on FrontEnd, need team to fix the logic

--- src/Pages/temp/components/CreateQuestionForm.jsx
+++ src/Pages/temp/components/CreateQuestionForm.jsx
@@ -1,6 +1,32 @@
 import React, { useState, useEffect } from "react";
 import "./CreateQuestionForm.css"
 
+function SelectBox({ numBoxes, onNumBoxesChange }) {
+  return (
+    <div>
+      <select id="num-boxes" className = "QuestionEditorText" onChange={onNumBoxesChange} value={numBoxes}>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+    </div>
+  );
+}
+
+function TextBoxes({ numBoxes }) {
+  const textBoxes = [];
+  for (let i = 0; i < numBoxes; i++) {
+    textBoxes.push(<input type="text" key={i} className="text-box" />);
+  }
+  return (
+    <div className="text-box-container">
+      {textBoxes}
+    </div>  
+  );
+}
+
 const CreateQuestionForm = ({ handlePostQuestion }) => {
   const [questionType, setQuestionType] = useState("multiple-choice");
   const [questionPrompt, setQuestionPrompt] = useState("");
@@ -40,6 +66,11 @@ const CreateQuestionForm = ({ handlePostQuestion }) => {
         alert("Error creating question");
       });
   };
+  const [numBoxes, setNumBoxes] = useState(1);
+
+  const handleSelectChange = (e) => {
+    setNumBoxes(parseInt(e.target.value));
+  }
 
   return (
     <form onSubmit={handleSubmit}>
@@ -59,10 +90,7 @@ const CreateQuestionForm = ({ handlePostQuestion }) => {
         </button>
 
         <h1>Number of Choices: </h1>
-        <input 
-          type = "text"
-          className="QuestionEditorText"
-        />
+        <SelectBox numBoxes={numBoxes} onNumBoxesChange={handleSelectChange} />
         <h1>Correct Answer: </h1>
         <input 
           type = "text"
@@ -78,9 +106,15 @@ const CreateQuestionForm = ({ handlePostQuestion }) => {
         </div>
 
         <div className="QuestionChoices">
+          <input 
+          type = "text"
+          className="questionBox"
+          placeholder="Type your Question Here"
+          />
+          <br/>
           {questionType === "multiple-choice" && (
           <>
-            {choices.map((choice, index) => (
+            {/* {choices.map((choice, index) => (
               <div key={`choice-${index}`}>
                 <input
                   type="text"
@@ -95,19 +129,23 @@ const CreateQuestionForm = ({ handlePostQuestion }) => {
             ))}
             <button type="button" onClick={addChoice}>
               Add choice
-            </button>
+            </button> */}
+            <TextBoxes numBoxes={numBoxes} />
           </>
         )}
 
         {questionType === "numeric-input" && (
+          <div className="text-box-container">
           <input
             type="number"
             value={numericAnswer}
             onChange={(e) => setNumericAnswer(e.target.value)}
+            className="NumericInput"
           />
+          </div>
         )}
-
-      <button type="submit">Add question</button>
+      <br/>
+      <button type="submit" className="buttonSubmit">Add question</button>
          
         </div>
       </div>
